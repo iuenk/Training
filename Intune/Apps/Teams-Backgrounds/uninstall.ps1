a01b35b6c9aeef47548bab60850b07969e51a229
Ivo Uenk <ivo.uenk@udirection.com>
2024-11-11 11:15:14 +0100

    s

---
+++ Intune/Apps/Teams-Backgrounds/uninstall.ps1
@@ -0,0 +1,32 @@
+﻿$PackageName = "Teams-new-Backgrounds"
+
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-uninstall.log" -Force
+
+try{
+    # Local Folder 
+    $TeamsBG_Folder = "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads"
+
+    # Reference IDs
+    $TeamsBG_ref = "$TeamsBG_Folder\TeamsBackgrounds.csv"
+
+    if(Test-Path $TeamsBG_ref){
+        # Clean up old files
+        $TeamsBG_old = Import-Csv -Path $TeamsBG_ref
+        foreach($TeamsBG in $TeamsBG_old.Name){
+            Write-Host "Removing $TeamsBG ..."
+            Remove-Item -Path "$TeamsBG_Folder\$TeamsBG*" -Force
+        }
+    }else{
+        Write-Host "No old files found"
+    }
+    
+
+    # Remove Detection Key
+    Remove-Item -Path "HKCU:\SOFTWARE\scloud\$PackageName" -Force
+
+    
+}catch{
+    Write-Error "$_"
+}
+
+Stop-Transcript
